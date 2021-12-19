048d48d99a54812eb8d2008c496409c4517dd500
vettorefburana <74903161+vettorefburana@users.noreply.github.com>
2021-12-19 16:36:35 +0100

    Add files via upload

---
+++ requirements.r
@@ -0,0 +1,13 @@
+
+
+install.packages("ggcorrplot")
+install.packages("MASS")
+install.packages("keras")
+install.packages("NeuralNetTools")
+install.packages("kableExtra")
+install.packages("fpc")
+install.packages("psych")
+install.packages("Hmisc")
+install.packages("cluster")
+install.packages("mclust")
+install.packages("factoextra")
